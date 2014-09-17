4dee2d2302ab16588a6bb849270134d6a7246b1d
FelisDJ <jackthip001@hotmail.com>
2014-09-17 12:42:01 +0700

    FirstCommit & ShowBackground

---
+++ Meow/src/MeowGame.java
@@ -0,0 +1,50 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+
+public class MeowGame extends BasicGame {
+
+	public static final int GAME_WIDTH = 640;
+	public static final int GAME_HEIGHT = 480;
+	Image backGround;
+	
+	public MeowGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		backGround.draw(0,0);
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		backGround = new Image("res/BackGround2.png");
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+		      MeowGame game = new MeowGame("Meow Game");
+		      AppGameContainer appgc = new AppGameContainer(game);
+		      appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
+		      appgc.setMinimumLogicUpdateInterval(1000 / 60);
+		      appgc.start();
+		    } catch (SlickException e) {
+		      e.printStackTrace();
+		    }
+
+	}
+}
